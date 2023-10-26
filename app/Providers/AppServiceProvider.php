863f06451b67082502a5c201c00889990e8d2793
hiroya12221023 <a1211065@kuins.ac.jp>
2023-10-26 14:21:54 +0000

    herokuにデプロイする準備

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+     public function boot()
     {
-        //
+        // asset()でURLを呼び出している個所でHTTPSでアクセスするようにする
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
